0039504190d159233580ec2ec3e5a91ac7546a0e
blackjac7 <febriansyah705@gmail.com>
2024-08-13 01:14:48 +0700

    dialect option off

--- config/sequelize.js
+++ config/sequelize.js
@@ -2,27 +2,24 @@ require("dotenv").config();
 
 module.exports = {
     development: {
-        url: process.env.DATABASE_URL,
+        username: process.env.POSTGRES_USERNAME,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+        host: process.env.POSTGRES_HOST,
         dialect: "postgres",
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
-        },
     },
     test: {
-        url: process.env.DATABASE_URL,
+        username: process.env.POSTGRES_USERNAME,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+        host: process.env.POSTGRES_HOST,
         dialect: "postgres",
     },
     production: {
-        url: process.env.DATABASE_URL,
+        username: process.env.POSTGRES_USERNAME,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+        host: process.env.POSTGRES_HOST,
         dialect: "postgres",
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
-        },
     },
 };
